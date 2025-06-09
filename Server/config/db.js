8662bb72a567290edf4df086a58904c94e9931f0
Shope Alli-B <zoeyalli921@gmail.com>
2025-06-08 18:41:43 -0700

    Initial commit

--- Server/config/db.js
+++ Server/config/db.js
@@ -3,6 +3,10 @@ require('dotenv').config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+   ssl: {
+    rejectUnauthorized: false
+  }
+  
 });
 
 module.exports = pool; 
