48590b2ed4d355813b9f85e2aab3313f39f4c636
Shope Alli-B <zoeyalli921@gmail.com>
2025-06-10 17:30:56 -0700

    Initial commit

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.get('*', (req, res) => {
 
 const PORT = process.env.PORT || 7700;
 
- app.listen(PORT, () => {
+ app.listen(PORT,  "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`); 
 });
 
