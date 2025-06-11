0915083b7cdd70bc5d2a1646d4e929f0a025265f
Shope Alli-B <zoeyalli921@gmail.com>
2025-06-10 17:34:48 -0700

    Initial commit

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swagger));
 
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'Create an account.html'));
 });
 
 
