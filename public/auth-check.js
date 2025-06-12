92dbb2c47c676c4c177f516b839d61f3df83b6d3
Shope Alli-B <zoeyalli921@gmail.com>
2025-06-11 17:52:32 -0700

    Initial commit

--- public/auth-check.js
+++ public/auth-check.js
@@ -4,7 +4,7 @@
   const token = localStorage.getItem('token');
   const path = window.location.pathname.substring(window.location.pathname.lastIndexOf('/') + 1).toLowerCase();
 
-  const openPages = ['sign in.html', 'index.html', 'Home.html'];
+  const openPages = ['Sign In.html', 'index.html', 'Home.html'];
   const protectedPages = ['Post-Property.html','My properties list.html','view properties.html','Searched properties.html'];
 
   
